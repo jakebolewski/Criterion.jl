d8a0f5ef3f518314f841cb4216bbeb79fecc3e16
Jake Bolewski <jakebolewski@gmail.com>
2013-10-16 13:13:37 -0400

    port over haskell's report templates

--- src/test_criterion.jl
+++ src/test_criterion.jl
@@ -12,15 +12,16 @@ env = Criterion.measure_environment()
 #times = Criterion.run_benchmakr(env, bench, false)
 times, sa, out = Criterion.run_analyze_one(env, "test_svd", bench)
 
-using Winston
 
-p = FramedPlot()
-setattr(p, "title", "Benchmark Times (sec)")
-setattr(p, "ylabel", "counts")
-setattr(p, "xlabel", "times (sec)")
-add(p, Histogram(hist(times, 100)...))
-add(p, LineX(sa.mean.point, "color", "red"))
-add(p, LineX(sa.mean.lbound, "type", "dot"))
-add(p, LineX(sa.mean.ubound, "type", "dot"))
-Winston.display(p)
+
+#using Winston
+#p = FramedPlot()
+#setattr(p, "title", "Benchmark Times (sec)")
+#setattr(p, "ylabel", "counts")
+#setattr(p, "xlabel", "times (sec)")
+#add(p, Histogram(hist(times, 100)...))
+#add(p, LineX(sa.mean.point, "color", "red"))
+#add(p, LineX(sa.mean.lbound, "type", "dot"))
+#add(p, LineX(sa.mean.ubound, "type", "dot"))
+#Winston.display(p)
 
